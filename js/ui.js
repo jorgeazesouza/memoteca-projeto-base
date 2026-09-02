773a86ddd998374bf8a8da49f744ae2e4503f47a
Jorge Azevedo <jorgeazesouza2306@gmail.com>
2026-09-02 15:07:30 -0300

    Ajustando a interface para realizar a edição dos pensamentos

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,13 @@
 import api from './api.js'; // importando api
 
 const ui = {
+   async preencherFormulario(pensamentoId) {
+      const pensamento = await api.buscarPensamentoPorId(pensamentoId);
+      document.querySelector('#pensamento-id').value = pensamento.id;
+      document.querySelector('#pensamento-conteudo').value = pensamento.conteudo;
+      document.querySelector('#pensamento-autoria').value = pensamento.autoria;
+   },
+
    async renderizarPensamentos() {
       try {
          // armazenando arquivo JSON
@@ -34,9 +41,23 @@ const ui = {
       pensamentoAutoria.textContent = pensamento.autoria;
       pensamentoAutoria.classList.add('pensamento-autoria');
 
+      const botaoEditar = document.createElement('button');
+      botaoEditar.classList.add('botao-editar');
+      botaoEditar.onclick = () => ui.preencherFormulario(pensamento.id);
+
+      const iconeEditar = document.createElement('img');
+      iconeEditar.src = 'assets/imagens/icone-editar.png';
+      iconeEditar.alt = 'Editar';
+      botaoEditar.appendChild(iconeEditar);
+
+      const icones = document.createElement('div');
+      icones.classList.add('icones');
+      icones.appendChild(botaoEditar);
+
       li.appendChild(iconeAspas);
       li.appendChild(pensamentoConteudo);
       li.appendChild(pensamentoAutoria);
+      li.appendChild(icones);
 
       listaPensamentos.appendChild(li);
    },
