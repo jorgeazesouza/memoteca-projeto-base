0acca97fa9008d81e3fb26bee3a85c1d8242843a
Jorge Azevedo <jorgeazesouza2306@gmail.com>
2026-09-02 10:00:49 -0300

    Adicionando dados com o POST

--- js/api.js
+++ js/api.js
@@ -5,12 +5,27 @@ const api = {
         try {
             // fetch() -  função nativa usada para fazer requisições HTTP de forma assíncrona
             // await - pausa a execução da função até que uma tarefa termine
-            const response = await fetch('http://localhost:3000/pensamentos');
+            const response = await fetch('http://localhost:3000/pensamentos'); // GET - Pegar
             return await response.json(); // Converte os dados para o formato JSON
         } catch (error) {
             alert('Erro ao buscar pensamentos.');
             throw error;
         }
+    },
+
+    async salvarPensamento(pensamento) {
+      try {
+         const response = await fetch('http://localhost:3000/pensamentos', {
+            method: "POST", // Enviar informação
+            headers: {
+               "Content-Type": "application/json" // informando o tipo de conteúdo
+            },
+            body: JSON.stringify(pensamento) // converte de obj p json
+         });
+      } catch (error) {
+         alert('Erro ao buscar pensamentos.');
+         throw error;
+      }
     }
 };
 
