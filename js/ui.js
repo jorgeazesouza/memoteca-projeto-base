1746e59280a4655a605f3082c2b89d7cca01a4ba
Jorge Azevedo <jorgeazesouza2306@gmail.com>
2026-09-02 10:11:33 -0300

    encapsulamento de adicionar pensamentos na lista

--- js/ui.js
+++ js/ui.js
@@ -2,25 +2,43 @@ import api from './api.js'; // importando api
 
 const ui = {
    async renderizarPensamentos() {
-      // armazendo ul do html
-      const listaPensamentos = document.querySelector('#lista-pensamentos');
-
       try {
          // armazenando arquivo JSON
          const pensamentos = await api.buscarPensamentos();
          // criando cada pensamento do JSON no html
-         pensamentos.forEach(pensamento => {
-            listaPensamentos.innerHTML += `
-               <li class="li-pensamento" data-id="${pensamento.id}">
-               <img src="assets/imagens/aspas-azuis.png" alt="Aspas azuis" class="icone-aspas">
-               <div class="pensamento-conteudo">${pensamento.conteudo}</div>
-               <div class="pensamento-autoria">${pensamento.autoria}</div>
-               </li>
-            `;
-         });
+         pensamentos.forEach(ui.adicionarPensamentoNaLista);
       } catch (error) {
          alert('Erro ao renderizar pensamentos');
       }
+   },
+
+   // Encapsulando adicionarPensamento
+   adicionarPensamentoNaLista(pensamento) {
+      // armazendo ul do html
+      const listaPensamentos = document.querySelector('#lista-pensamentos');
+
+      const li = document.createElement('li');
+      li.setAttribute('data-id', pensamento.id);
+      li.classList.add('li-pensamento');
+
+      const iconeAspas = document.createElement('img');
+      iconeAspas.src = 'assets/imagens/aspas-azuis.png';
+      iconeAspas.alt = 'Aspas azuis';
+      iconeAspas.classList.add('icone-aspas');
+
+      const pensamentoConteudo = document.createElement('div');
+      pensamentoConteudo.textContent = pensamento.conteudo;
+      pensamentoConteudo.classList.add('pensamento-conteudo');
+
+      const pensamentoAutoria = document.createElement('div');
+      pensamentoAutoria.textContent = pensamento.autoria;
+      pensamentoAutoria.classList.add('pensamento-autoria');
+
+      li.appendChild(iconeAspas);
+      li.appendChild(pensamentoConteudo);
+      li.appendChild(pensamentoAutoria);
+
+      listaPensamentos.appendChild(li);
    }
 };
 
