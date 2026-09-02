22eef6db0fdc3c3d52576e20ffb4c3aa4775e1f5
Jorge Azevedo <jorgeazesouza2306@gmail.com>
2026-09-02 10:22:16 -0300

    submetendo formulário para criar uma novo dado

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 import ui from './ui.js';
+import api from './api.js';
 
 // adicionar um evento quando a página terminar de carregar
 document.addEventListener('DOMContentLoaded', () => {
    ui.renderizarPensamentos(); // renderizando pensamentos na pagina
+
+   const formularioPensamento = document.querySelector('#pensamento-form');
+   formularioPensamento.addEventListener('submit', manipularSubmissaoFormulario);
 });
+
+async function manipularSubmissaoFormulario(event) {
+   event.preventDefault();
+   const id = document.querySelector('#pensamento-id').value;
+   const conteudo = document.querySelector('#pensamento-conteudo').value;
+   const autoria = document.querySelector('#pensamento-autoria').value;
+
+   try {
+      await api.salvarPensamento({ conteudo, autoria });
+      ui.renderizarPensamentos();
+   } catch (error) {
+      alert('Erro ao salvar pensamentos');
+   }
+}
